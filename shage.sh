89c426d91d5df40696656927e81647597dd93932
Wenxing Hu <whu1@edge-hpc-gpu-105.cm.cluster>
2025-06-09 13:04:21 -0400

    update sh

--- shage.sh
+++ shage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-#SBATCH --job-name=age60
+#SBATCH --job-name=age
 #SBATCH -p gpu
 #SBATCH --gres=gpu:1
 #SBATCH -t 50-00:00:00 
@@ -7,8 +7,8 @@
 #SBATCH --ntasks-per-node=1 
 #SBATCH --cpus-per-task=4
 #SBATCH -a 0-19 
-#SBATCH -o age.regnety_32.s98.sub60.v%a.out
-#SBATCH -e age.regnety_32.s98.sub60.v%a.err
+#SBATCH -o age.regnety_32.v%a.out
+#SBATCH -e age.regnety_32.v%a.err
 module load anaconda3 
 conda activate /edgehpc/dept/compbio/users/whu1/envs/tmpenv
 python src/main_age.py --seed 98 --model regnety_32 --imshape 390_wx --batch 16 --epoch 17  --lrate 3e-5 --loss_type MAE --pheno age --version $SLURM_ARRAY_TASK_ID --subsample 60 --r_degree 33
